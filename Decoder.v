0b03622f6bb89de2bc53285e02165e9523e569d9
Hanbo <hanbog2@illinois.edu>
2021-12-08 04:19:36 +0800

    Completed without debugging

--- Decoder.v
+++ Decoder.v
@@ -1,26 +1,28 @@
 module decoder(
-    input reg  [1:0] sw,
+    input      [1:0] sw,
     input            clk,
     input      	  reset,
     output reg [4:0] morse
 );
 
-reg prev_sw[1:0];
-reg temp_morse[4:0];
-assign prev_sw = sw;
-integer i;
-assign i = 4;
+reg [1:0] prev_sw;
+reg [4:0] temp_morse;
+
+integer i = 4;
+
 always @(posedge clk) begin
     if(sw[0] != prev_sw[0]) begin
         temp_morse[i] = 1'b0;
         i = i - 1;
+        prev_sw = sw;
     end
     if(sw[1] != prev_sw[1]) begin
         temp_morse[i] = 1'b1;
         i = i - 1;
+        prev_sw = sw;
     end
     if(i == 0) begin
-        assign morse[4:0] = temp_morse[4:0];
+        morse = temp_morse;
         i = 4;
     end
 end
